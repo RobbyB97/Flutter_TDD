97ae3800ae6bbb41c2b90099a6667573bd517461
Robert Bergers <bergersr97@gmail.com>
2020-11-07 14:11:16 -0500

    get random trivia number usecase initial commit

---
+++ flutter_tdd/test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -0,0 +1,37 @@
+import 'package:dartz/dartz.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_tdd/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:flutter_tdd/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'package:flutter_tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class MockNumberTriviaRepository extends Mock
+    implements NumberTriviaRepository {}
+
+void main() {
+  GetRandomNumberTrivia usecase;
+  MockNumberTriviaRepository mockNumberTriviaRepository;
+
+  setUp(() {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    usecase = GetRandomNumberTrivia(mockNumberTriviaRepository);
+  });
+
+  final tNumberTrivia = NumberTrivia(text: 'test', number: 1);
+
+  test(
+    'should get trivia from the repository',
+    () async {
+      //  arrange  (Return future object. left side failure, right side success)
+      when(mockNumberTriviaRepository.getRandomNumberTrivia())
+          .thenAnswer((_) async => Right(tNumberTrivia));
+      //  act
+      final result = await usecase(NoParams());
+      // assert
+      expect(result, Right(tNumberTrivia));
+      verify(mockNumberTriviaRepository.getRandomNumberTrivia());
+      verifyNoMoreInteractions(mockNumberTriviaRepository);
+    },
+  );
+}
