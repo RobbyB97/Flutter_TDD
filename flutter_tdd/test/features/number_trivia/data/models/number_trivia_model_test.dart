d9ce5e7df3f111a9c1bf3162db09b68270a1a046
Robert Bergers <bergersr97@gmail.com>
2020-11-10 16:44:50 -0500

    Test number trivia model if number is an integer

--- flutter_tdd/test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ flutter_tdd/test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_tdd/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:flutter_tdd/features/number_trivia/domain/entities/number_trivia.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
 
 void main() {
   final tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'Test text');
@@ -9,5 +13,17 @@ void main() {
   test('Should be a subclass of NumberTrivia entity', () async {
     //assert
     expect(tNumberTriviaModel, isA<NumberTrivia>());
+  });
+
+  group('fromJson', () {
+    test('Should return a valid model when the JSON number is an integer',
+        () async {
+      //arrange
+      final Map<String, dynamic> jsonMap = json.decode(fixture('trivia.json'));
+      //act
+      final result = NumberTriviaModel.fromJson(jsonMap);
+      //assert
+      expect(result, tNumberTriviaModel);
+    });
   });
 }
